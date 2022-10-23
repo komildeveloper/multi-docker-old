c3369eb3ae2cd095237443051e8228456cda03ea
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-10-23 18:13:41 +0500

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className='App-title'>Fib Calculator</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
